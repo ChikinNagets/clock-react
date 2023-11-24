c434f256b2b8cf12f1c56736cf9b342b193edf80
ChikinNagets <ing.calderonh@gmail.com>
2023-11-25 01:45:47 +0900

    RCP-1: Added Basic project
    Created basic redux vite project with the clock component.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import ClockComponent from './components/ClockComponent'
+
+function App() {
+  return (
+    <div>
+      <p>Welcome to your clock</p>
+      <ClockComponent/>
+    </div>
+  )
+}
+
+export default App
